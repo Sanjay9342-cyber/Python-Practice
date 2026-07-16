3c0aee4081f6a3c3c6591a2d85459f2544a9d11c
Sanjay9342-cyber <sanjaysabari118@gmail.com>
2026-07-16 11:09:28 +0530

    Create Day_identifier.py
    s

---
+++ If_Else/Day_identifier.py
@@ -0,0 +1,17 @@
+D=int(input())
+if D==1:
+    print("Sunday")
+elif D==2:
+    print("Monday")
+elif D==3:
+    print("Tuesday")
+elif D==4:
+    print("Wednesday")
+elif D==5:
+    print("Thursday")
+elif D==6:
+    print("Friday")
+elif D==7:
+    print("Saturday")
+else:
+    print("Invalid Day")
